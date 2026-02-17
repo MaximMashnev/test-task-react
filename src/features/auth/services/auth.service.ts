b52153f887c9f327c020c271cee7ef041c6d7c57
MaximMashnev <maximmashnev345@gmail.com>
2026-02-18 01:01:20 +0300

    Добавлена модель пользователя, сделан сервис аутетификации, вынесены константы

---
+++ src/features/auth/services/auth.service.ts
@@ -0,0 +1,30 @@
+import config from "../../../shared/configs/config.json";
+import { User as UserEntity } from "../../../entities/User";
+import { httpService } from "../../../shared/api/services/http.service";
+
+const authMe = config.api.endPoints.auth_me;
+const auth = config.api.endPoints.auth;
+
+interface loginData {
+    data: Omit<UserEntity, "password">;
+    token: string;
+}
+
+type LoginForm = Pick<UserEntity, "login" | "password">;
+type AuthMe = Omit<UserEntity, "password">;
+
+const authService = {
+    async login (
+        loginForm: LoginForm
+    ) {
+        const { data } = await httpService.post<loginData>(auth, loginForm);
+        return data;
+    },
+
+    async authMe () {
+        const { data } = await httpService.get<AuthMe>(authMe);
+        return data;
+    }
+}
+
+export default authService;
