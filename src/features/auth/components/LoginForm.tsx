9acabf317faa45af7cd1b8f64cf76c8c3b48fa55
MaximMashnev <maximmashnev345@gmail.com>
2026-02-18 01:02:24 +0300

    Сделана форма входа, добавлен интерсептор

--- src/features/auth/components/LoginForm.tsx
+++ src/features/auth/components/LoginForm.tsx
@@ -1,17 +1,52 @@
-import { Box, Button, TextField, Typography } from "@mui/material";
+import { Alert, Box, Button, TextField, Typography, IconButton } from "@mui/material";
 import Container from "../../../shared/ui/Container";
 import { useState } from "react";
+import bgImage from "../../../shared/assets/imgs/bgImage.jpg";
+import { useNavigate } from "react-router";
+import authService from "../services";
+import { STORAGE_KEYS } from "../../../shared/consts";
+import HomeIcon from '@mui/icons-material/Home';
 
 export default function LoginForm () {
 
-    const [login, setLogin] = useState<string>("");
-    const [password, setPassword] = useState<string>("");
+    let navigate = useNavigate();
 
-    const test: boolean = false;
+    const [isLoading, setIsLoading] = useState(false);
+    const [login, setLogin] = useState("");
+    const [password, setPassword] = useState("");
+    const [error, setError] = useState("");
+    const [touched, setTouched] = useState({login: false, password: false})
 
-    function handleOnLogin() {
-        console.log(login);
-        console.log(password);
+    const isLoginValid = login.length >= 4;
+    const isPasswordValid = password.length >= 8;
+
+    const showLoginError = touched.login && !isLoginValid;
+    const showPasswordError = touched.password && !isPasswordValid;
+
+    const authUser = () => {
+        setIsLoading(true);
+        setError("");
+        authService.login({login, password})
+            .then(
+                (data) => {
+                    localStorage.setItem(STORAGE_KEYS.TOKEN, data.token);
+                    localStorage.setItem(STORAGE_KEYS.USER, JSON.stringify(data.data));
+                    navigate("/");                
+            })
+            .catch(
+                (error) => {
+                    const message = error.response.data.message ?? "Ошибка авторизации";
+                    setError(message);                
+            })
+            .finally(
+                () => {
+                    setIsLoading(false);
+            })
+    }   
+
+    const handleSubmit = (e: React.SubmitEvent) => {
+        e.preventDefault();
+        authUser();
     }
 
     return (
@@ -19,38 +54,75 @@ export default function LoginForm () {
             sx={{ 
                 display: "flex",
                 flexDirection: "column",
-                gap: "1rem",
                 alignItems: "center",
                 justifyContent: "center",
-                height: "100vh"
+                height: "100vh",
+                position: "relative", 
+                '&::before': {
+                    content: '""',
+                    position: "absolute",
+                    top: 0,
+                    left: 0,
+                    width: "100%",
+                    height: "100%",
+                    backgroundImage: `url(${bgImage})`,
+                    backgroundSize: "cover",
+                    backgroundPosition: "center",
+                    filter: "blur(2px)",
+                    zIndex: -1,
+                }
             }}>
             <Container>
-                <Typography variant="h6" component="span">
-                    Авторизация
-                </Typography>
-                <TextField
-                    error={test}
-                    value={login}
-                    onChange={e => setLogin(e.target.value)}
-                    id="login"
-                    label="Логин"
-                    placeholder="example@ex.com"
-                    helperText={test && "Неверный ввод."}
-                    type="email"
-                />
-                <TextField
-                    error={test}
-                    value={password}
-                    onChange={e => setPassword(e.target.value)}
-                    id="password"
-                    label="Пароль"
-                    placeholder="Password"
-                    helperText={test && "Неверный ввод."}
-                    type="password"
-                />
-                <Button variant="contained" onClick={handleOnLogin}>
-                    Войти
-                </Button>
+                <Box sx={{display: "flex", justifyContent: "space-between"}}>
+                    <Typography variant="h6" component="span">
+                        Авторизация
+                    </Typography>
+                    <IconButton href="/" title="На главную">
+                        <HomeIcon />
+                    </IconButton>
+                </Box>
+                {error && <Alert severity="error">{error}</Alert>}
+                <Box 
+                    onSubmit={handleSubmit}
+                    component="form" 
+                    sx={{
+                        display: "flex",
+                        width: "100%",
+                        flexDirection: "column",
+                        gap: "1rem"
+                    }}
+                >
+                    <TextField
+                        value={login}
+                        onChange={e => setLogin(e.target.value)}
+                        label="Логин"
+                        placeholder="example@ex.com"
+                        onBlur={() => {setTouched(prev => ({ ...prev, login: true}))}}
+                        error={showLoginError}
+                        helperText={showLoginError && "Минимальная длина 4 символа"}
+                        type="email"
+                        disabled={isLoading}
+                    />
+                    <TextField
+                        value={password}
+                        onChange={e => setPassword(e.target.value)}
+                        label="Пароль"
+                        placeholder="Password"
+                        onBlur={() => {setTouched(prev => ({ ...prev, password: true}))}}
+                        error={showPasswordError}
+                        helperText={showPasswordError && "Минимальная длина 8 символов"}
+                        type="password"
+                        disabled={isLoading}
+                    />
+                    <Button 
+                        type="submit" 
+                        variant="contained" 
+                        loading={isLoading}
+                        disabled={(isLoginValid && isPasswordValid) ? false : true}
+                    >
+                        Войти
+                    </Button>                    
+                </Box>    
             </Container>
         </Box>
     )
--- src/shared/api/services/http.service.ts
+++ src/shared/api/services/http.service.ts
@@ -1,6 +1,15 @@
 import axios from "axios";
 import config from "../../configs/config.json"
+import { STORAGE_KEYS } from "../../consts";
 
 export const httpService = axios.create({
     baseURL: config.api.baseUrl
+})
+
+httpService.interceptors.request.use((config) => {
+    const token = localStorage.getItem(STORAGE_KEYS.TOKEN);
+    if (token) {
+        config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
 })
