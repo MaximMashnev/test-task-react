67874291ab0a5ed48f4807cdb3705ae071fe617d
MaximMashnev <maximmashnev345@gmail.com>
2026-02-22 23:33:03 +0300

    + Интерфейс пагинации и мета

---
+++ src/shared/api/services/types.ts
@@ -0,0 +1,12 @@
+export interface Pagination {
+  page: number;
+  pageSize: number;
+}
+
+export interface Meta {
+    current_page: number;
+    per_page: number;
+    remaining_count: number;
+    total_items: number;
+    total_pages: number;
+}
